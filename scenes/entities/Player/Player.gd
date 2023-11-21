df001d513145b3ae9a0f557629348f2912043a8d
Hisoka-Morow-404 <hisokamorowu.u@gmail.com>
2023-11-21 22:07:14 +0100

    add weapon

--- scenes/entities/Player/Player.gd
+++ scenes/entities/Player/Player.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+func _ready():
+	var weapon = preload("res://scenes/entities/Weapon/Weapon.tscn").instantiate()
+	get_node("WeaponPosition/Marker2D").add_child(weapon);
+
 
 func _physics_process(delta):
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionX = Input.get_axis("ui_left", "ui_right")
 	var directionY = Input.get_axis("ui_up", "ui_down")
 	if directionX:
