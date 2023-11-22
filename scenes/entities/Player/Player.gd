9c9fd7ea267bb9a3ff4a8772dff17c48ad1017a1
Peppe <peppe@peppe.com>
2023-11-22 23:38:32 +0100

    bullet free queue on collision

--- scenes/entities/Player/Player.gd
+++ scenes/entities/Player/Player.gd
@@ -9,11 +9,12 @@ func _ready():
 	var weapon = preload("res://scenes/entities/Weapon/Weapon.tscn").instantiate()
 	get_node("WeaponPosition/Marker2D").add_child(weapon);
 	timer.one_shot = true
-	timer.wait_time = 1
+	timer.wait_time = 0.1
 	add_child(timer)
 
 
 func _physics_process(delta):
+	
 
 	var directionX = Input.get_axis("ui_left", "ui_right")
 	var directionY = Input.get_axis("ui_up", "ui_down")
@@ -40,7 +41,6 @@ func _physics_process(delta):
 	move_and_slide()
 	if(Input.is_mouse_button_pressed(1) && timer.is_stopped()):
 		timer.start()
-		print(timer.is_stopped())
 		generate_bullet()
 	
 func generate_bullet():
--- scenes/entities/bullet/StandardBullet/StandardBullet.gd
+++ scenes/entities/bullet/StandardBullet/StandardBullet.gd
@@ -8,3 +8,8 @@ var bullet_direction = Vector2()
 func _physics_process(delta):
 	velocity = bullet_direction * SPEED 
 	move_and_slide()
+	
+
+func _on_hit_box_body_entered(body):
+	if body is StaticBody2D:
+		queue_free()
