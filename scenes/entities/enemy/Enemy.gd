aa9311eb8d9aef419c41623d075b82422e1ba2bf
Hisoka-Morow-404 <hisokamorowu.u@gmail.com>
2023-11-23 00:02:53 +0100

    fix enemy

--- scenes/entities/enemy/Enemy.gd
+++ scenes/entities/enemy/Enemy.gd
@@ -5,7 +5,22 @@ extends CharacterBody2D
 const SPEED = 300.0
 var value = randf_range(-1, 1)
 var direction = Vector2(value,value).normalized()
+var timer = Timer.new()
+
+func _ready():
+	timer.one_shot = true
+	timer.wait_time = 3
+	
 
 func _physics_process(delta):
-	position += direction * SPEED * delta
+	if(timer.is_stopped()):
+		timer.start()
+		changeDirection(-10, 10)
+		velocity += direction * SPEED * delta
+		
+		
+	move_and_slide()
 
+func changeDirection(a:int, b:int):
+	value = randf_range(a, b)
+	var direction = Vector2(value,value).normalized()
