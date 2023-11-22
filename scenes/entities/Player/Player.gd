8c363653d28c2b539fcbee8544e2655de195d4cb
Hisoka-Morow-404 <hisokamorowu.u@gmail.com>
2023-11-22 13:16:09 +0100

    add timer

--- scenes/entities/Player/Player.gd
+++ scenes/entities/Player/Player.gd
@@ -2,16 +2,24 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+var timer = Timer.new()
+
 
 func _ready():
 	var weapon = preload("res://scenes/entities/Weapon/Weapon.tscn").instantiate()
 	get_node("WeaponPosition/Marker2D").add_child(weapon);
+	timer.one_shot = true
+	timer.wait_time = 1
+	add_child(timer)
 
 
 func _physics_process(delta):
 
 	var directionX = Input.get_axis("ui_left", "ui_right")
 	var directionY = Input.get_axis("ui_up", "ui_down")
+	
+
+
 	if directionX:
 		velocity.x = directionX * SPEED
 	else:
@@ -22,12 +30,13 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		
 	move_and_slide()
-	if(Input.is_mouse_button_pressed(1)):
+	if(Input.is_mouse_button_pressed(1) && timer.is_stopped()):
+		timer.start()
+		print(timer.is_stopped())
 		generate_bullet()
 	
 func generate_bullet():
 	var marker:Node2D = get_node("WeaponPosition/Marker2D")
-	var weapon_position:Node2D = get_node("WeaponPosition")
 	var bullet_standard = load("res://scenes/entities/bullet/StandardBullet/StandardBullet.tscn").instantiate()
 	bullet_standard.bullet_direction = (get_global_mouse_position() - marker.global_position).normalized()
 	bullet_standard.position = marker.global_position
